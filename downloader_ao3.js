5ddb1fca66efeb855b1026cebbccca879face462
Ivavgunevhf <59344892+Ivavgunevhf@users.noreply.github.com>
2020-08-28 22:37:39 -0500

    Update downloader_ao3.js
    Added support for the '/bookmarks' pages.

--- downloader_ao3.js
+++ downloader_ao3.js
@@ -1,6 +1,5 @@
-// Ao3 download from the works pages. 
+// AO3 download from the works pages. 
 // REQUIRES: jQuery
-// the rest of the code if you dont already have it
 
 // Date: Aug 28, 2020
 
@@ -17,6 +16,12 @@
 // On page fail it should stay as a tab. 
 // Meddle around with the code or manually do it.
 
+
+// Usage: You can use the it with the setting/UI at the top 
+// of the page or by calling the function: 
+// AO3Download('html');
+// or whatever file type you want that AO3 supports.
+
 //----------
 jQuery(function() {
 	if (jQuery('#ao3_downloader').length==0) { // Doesnt exist
@@ -24,12 +29,20 @@ jQuery(function() {
 		jQuery('body').prepend('<textarea id="downloaded_works"></textarea><textarea id="downloaded_works_names"></textarea>');
 		jQuery('#downloaded_works,#downloaded_works_names').hide();
 
-		jQuery('head').append('<style id="ao3_downloader_css">#ao3_downloader{border:1px solid;padding:15px;}.do_not_download{opacity:0.3;}</style>');
+		jQuery('head').append('<style id="ao3_downloader_css">#ao3_downloader{border:1px solid;padding:15px;}.do_not_download{opacity:0.3!important;}</style>');
 
-		jQuery('#main').prepend('<div id="ao3_downloader"><h2>AO3 Downloader by <a href="https://github.com/ivavgunevhf">ivavgunevhf</a></h2><p><strong>What does this tool do?</strong><br>--> It quickly downloads all the works seen on the page.<br>--> It also downloads two text files.<br>----> A text file with the work&apos;s title and the author&apos;s name.<br>----> A more detailed file about the work&apos;s title, author, and summary.<br><br><strong>Important</strong>: <br>--> Please <u>allow for multiple downloads</u> and <u>allow for pop-ups</u> when using this tool. <br>----> Because of the nature of how this code works, you must allow for pop-ups because this code opens multiple windows to the downloads.<br>----> Likewise, the multiple downloads is to allow the files to be downloaded to your computer.<br><br><strong>Tips:</strong><br>1. Set the downloads location to the correct folder before using this tool. (or plan on picking files out of your downloads folder for months)<br>2. Use the filters to remove the works you dont want to accidently download. (Curate the works before downloading everything.)<br>---> Use the "Exclude this work from the download" button to remove the work from the downloads list. (Excluded works are more transparent and greyed out.)</p>Download as: <select name="download_type" id="download_type" style="width: auto;vertical-align: middle;"><option value="html">html</option><option value="epub">epub</option><option value="pdf">pdf</option><option value="mobi">mobi</option><option value="azw3">azw3</option></select> <button id="downloader_button">Download the works on this page</button></div>');
+		jQuery('#main').prepend('<div id="ao3_downloader"><h2>AO3 Downloader by <a href="https://github.com/Ivavgunevhf/downloaders/">Ivavgunevhf</a></h2><p><strong>What does this tool do?</strong><br>--> It quickly downloads all the works seen on the page.<br>--> It also downloads two text files.<br>----> A text file with the work&apos;s title and the author&apos;s name.<br>----> A more detailed file about the work&apos;s title, author, and summary.<br>--> Works on most pages: /works, /bookmarks, /series<br>----> If you have a bookmarked series, you must visit the page that page to download it.<br>----> Will not work on external bookmarks.<br><br><strong>Important</strong>: <br>--> Please <u>allow for multiple downloads</u> and <u>allow for pop-ups</u> when using this tool. <br>----> Because of the nature of how this code works, you must allow for pop-ups because this code opens multiple windows to the downloads.<br>----> Likewise, the multiple downloads is to allow the files to be downloaded to your computer.<br><br><strong>Tips:</strong><br>1. Set the downloads location to the correct folder before using this tool. (or plan on picking files out of your downloads folder for months)<br>2. Use the filters to remove the works you dont want to accidentlly download. (Curate the works before downloading everything.)<br>---> Use the "Exclude this work from the download" button to remove the work from the downloads list. (Excluded works are more transparent and greyed out.)</p>Download as: <select name="download_type" id="download_type" style="width: auto;vertical-align: middle;"><option value="html">html</option><option value="epub">epub</option><option value="pdf">pdf</option><option value="mobi">mobi</option><option value="azw3">azw3</option></select> <button id="downloader_button">Download the works on this page</button></div>');
 
-		jQuery('li.work').each(function(){
-			jQuery(this).prepend('<button class="downloadable_toggler">Exclude this work from the download</button>');
+		jQuery('li.work, li.bookmark').each(function(){
+			if (jQuery(this).find('h4 a').length>0) {//Only works that exist
+				if (jQuery(this).find('h4 a').attr('href').indexOf('series')>=0) { // Series
+					jQuery(this).addClass('do_not_download').prepend('Must go to the series page to download.')
+				} else if (jQuery(this).find('.notice').length>0) { // External
+					jQuery(this).addClass('do_not_download').prepend('Cannot download external works.')
+				} else { // Normal things
+					jQuery(this).prepend('<button class="downloadable_toggler">Exclude this work from the download</button>');
+				}
+			}
 		});
 
 		// Add listeners
@@ -50,7 +63,10 @@ jQuery(function() {
 
 
 function AO3Download(type) {
-	// For Each
+	// Clear the textareas to avoid multiple entries from past runs
+	jQuery('#downloaded_works,#downloaded_works_names').val('');
+	
+	// Page: /works
 	jQuery('li.work').each(function(){
 		if (jQuery(this).hasClass('do_not_download')!==true) {
 			// Find the work id for each story on the page
@@ -87,6 +103,43 @@ function AO3Download(type) {
 		}
 	});
 
+	// Page: /bookmarks
+	jQuery('li.bookmark').each(function(){
+		if (jQuery(this).hasClass('do_not_download')!==true&&jQuery(this).find('h4 a').length>0) {
+			// Prevents deleted works bookmarks from glitching the code
+			
+			// Find the work id for each story on the page
+			var id = jQuery(this).find('h4 a').attr('href').split('/works/')[1];
+			// Title isnt important or so i think? but grab it anyways
+			var title = jQuery(this).find('h4 a').first().text();
+			// Author too
+			var author = jQuery(this).find('h4 a').first().next().text();
+			var byline = jQuery(this).find('h4.heading').first().text().replace(/\r?\n|\r|»/g,"").replace(/\s\s+/g, ' ').trim(); // Yeah regex is messy but whatever
+			var summary = jQuery(this).find('.summary').text();
+
+			
+			// AO3 Downloads currently works as this, so far i cant tell the differnce from not having the title name there but 
+			// I might as well have it there just in case. I think rwqerjklo.html would also continue to work for it with the 
+			// id but meh.
+			
+			//download_link = 'http://download.archiveofourown.org/downloads/'+id+'/'+title.replace(/ /g,"%20").replace(/'|:|\"|!|\.|\?/g,"")+'.html';
+			
+			// This might be more reliable? bc ao3 doesnt seem to care what i place there.
+			var download_link = 'http://download.archiveofourown.org/downloads/'+id+'/somerandomunimportantname.'+type;
+			
+			jQuery('#downloaded_works').val(jQuery('#downloaded_works').val()+byline+'\n'+summary+'\nAo3 id:'+id+'\n---------\n');
+			jQuery('#downloaded_works_names').val(jQuery('#downloaded_works_names').val()+title+' by '+author+'\n');
+
+			// Display in the console
+			console.log(title, download_link, id);
+			
+			// !!! IMPORTANT !!!
+
+			// bc of the window open command, you must allow for popups on the site
+			// command is commented out bc i dont want autorun when debugging remove the // from the next line to make it run.
+				window.open(download_link,'_blank');
+		}
+	});
 	// Download the info
 	// Summaries are the title and the summary data
 	// TITLES are just the title no other info NO author names. 
